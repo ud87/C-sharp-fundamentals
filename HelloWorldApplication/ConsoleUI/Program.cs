5abdd01b79ba83c4d489afb7e37dae68943465d9
Udaya <udayarai@hotmail.co.uk>
2024-09-29 22:35:18 +0100

    Initial commit

---
+++ HelloWorldApplication/ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI   //namespace is project name by default
+{
+    internal class Program
+    {
+        static void Main(string[] args)   //static void main is a method which is a code that runs everything inside the { }, compiler knows where to start the app
+        {
+            Console.Beep();      //Beeps and makes sound 
+            Console.Write("Computer says: ");
+            Console.WriteLine("Hello World!");   //console is a class which holds a bunch of methods, .writeLine is a method which outputs the string
+            //all methods have ( ) and all C# code line ends in ;
+            Console.ReadLine();             //waits until user inputs something, way to collect a user input
+
+
+            //whenever we need Intellisence we can press Ctrl + j
+            
+
+        }
+    }
+}
