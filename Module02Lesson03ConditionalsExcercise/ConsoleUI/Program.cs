82d90ed3ff3e185ed4624e38fdc56baa0c9c6722
Udaya <udayarai@hotmail.co.uk>
2024-10-01 20:26:32 +0100

    Initial upload

---
+++ Module02Lesson03ConditionalsExcercise/ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            if (firstName.ToLower() == "tim" &&  lastName.ToLower() =="corey")
+            {
+                Console.WriteLine("Welcome professor");
+            }
+            else
+            {
+                Console.WriteLine("Welcome students");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/*
+ Create a console application that asks the user for their name. Welcome me(Tim) as professor or anyone else as a student
+ */
