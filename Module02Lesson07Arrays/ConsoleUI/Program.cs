c736caaf8d8e5ce75cc78ede4f25f159d88aa200
Udaya <udayarai@hotmail.co.uk>
2024-10-03 23:25:11 +0100

    Initial commit

---
+++ Module02Lesson07Arrays/ConsoleUI/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] firstNames = new string[5];   //new indicates create something new and 5 means how many items in array
+
+            //firstNames[0] = "Udaya";        //which position in array, array is zero based
+            //firstNames[1] = "Tim";
+            //firstNames[2] = "Harry";
+            //Console.WriteLine($"My array has {firstNames[0]}, {firstNames[1]}, {firstNames[2]}}");
+
+
+            //string data = "Tim,Sue,Bob,Frank,Sandra, Molly";
+
+            //string[] firstNames = data.Split(',');
+
+            //Console.WriteLine($"The third first name is {firstNames[2]}");
+
+
+            //int[] ages = new int[3];
+            //ages[0] = 4;
+            //ages[1] = 5;
+            //ages[2] = 6;
+
+
+            //Array initialization syntax
+            //string[] lastNames = new string[] {"Corey", "Smith", "Jones" };
+            //lastNames[0] = "Udaya";     //we can overwrite  arrays
+
+            //Console.WriteLine($"{lastNames[0]}");
+
+
+
+
+            //string sentence = "Array initialization with new keyword and size.";
+            //string[] words = sentence.Split(' ');
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //}
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/*
+ Array is a set of data which can be accessed using a numerical index
+ Best to use arrays when we know how big the data is 
+ or when data is already filled
+ */
