35a32b5561071f25de528c5cb0524da0065333b6
unknown <udayarai@hotmail.co.uk>
2024-10-08 23:26:14 +0100

    Initial commit

---
+++ Module02Lesson10ForLoop/ConsoleUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //string[] firstNames = new string[] { "udaya", "Renuka", "Manoj", "Harry" };
+
+            //for (int i = 0; i < firstNames.Length; i++)
+            //{
+            //    Console.WriteLine(firstNames[i]);
+            //}
+
+
+            List<string> firstNames = new List<string>();
+            firstNames.Add("Mahesh");
+            firstNames.Add("Harry");
+            firstNames.Add("Ronda");
+            firstNames.Add("Rilesh");
+            firstNames.Add("Nilesh");
+
+            for (int i = 0; i < firstNames.Count; i++)
+            {
+                Console.WriteLine(firstNames[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module02Lesson10ForLoopExcercise/ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for firstNamesCommaSeperated
+            Console.Write("Please enter first names seperated by comma: ");
+            string firstNamesCommaSeperated = Console.ReadLine();
+            //remove all line break chars like space, tabs, linebreaks and also remove , from beginning and end, this ensures that the input is not blank
+            firstNamesCommaSeperated = Regex.Replace(firstNamesCommaSeperated, @"\s", "").Trim(','); 
+
+            while (firstNamesCommaSeperated == "")
+            {
+                Console.Write("User input cannot be empty. Please re-enter first names seperated by comma: ");
+                firstNamesCommaSeperated = Console.ReadLine();
+                firstNamesCommaSeperated = Regex.Replace(firstNamesCommaSeperated, @"\s", "").Trim(',');
+            }
+            
+
+            //remove , from start and end, split into firstNamesSeperated
+            string[] firstNames = firstNamesCommaSeperated.Trim(',').Split(',');
+
+            //loop through the array and say hello
+            for (int i = 0; i < firstNames.Length; i++) 
+            {
+                if (firstNames[i] != "")    //ignore empty array elements
+                {
+                    Console.WriteLine($"Hello {firstNames[i]}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+//Ask a user for a comma seperated list of firstnames (no spaces)
+//Split the string into string array
+//Loop through the array and tell each person hello
