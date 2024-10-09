fed418c52dca5d53a3ed0514906047accbd2f6de
Udaya <udayarai@hotmail.co.uk>
2024-10-09 23:30:04 +0100

    Initial commit

---
+++ Module02Lesson11ForEach/ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> firstNames = new List<string>();
+
+            //firstNames.Add("Tim");
+            //firstNames.Add("Sue");
+            //firstNames.Add("Bob");
+            //firstNames.Add("Dan");
+
+            //foreach (string name in firstNames)
+            //{
+            //    Console.WriteLine($"Hello {name}");
+            //}
+
+            Dictionary<int, string> importantYears = new Dictionary<int, string>();
+
+            importantYears.Add(1942, "Columbus arrives in Central America");
+            importantYears.Add(1969, "Man walks on the moon");
+            importantYears.Add(1987, "Udaya is born");
+
+            foreach (var year in importantYears) 
+            {
+                Console.WriteLine($"{year.Key} : {year.Value}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/*
+ Whenever you can please use foreach for list 
+ if you really need to use counter you can 
+ 
+ */
