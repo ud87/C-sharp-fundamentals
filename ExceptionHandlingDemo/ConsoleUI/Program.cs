c292daaa97ee291f4bd48deccd29e99c8ae7734d
Udaya <udayarai@hotmail.co.uk>
2024-10-15 23:50:09 +0100

    Initial commit

---
+++ ExceptionHandlingDemo/ConsoleUI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                BadCall();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("There was an exception thrown in our app");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+        private static void BadCall()
+        {
+            int[] ages = new int[] { 1, 3, 5 };
+
+            for (int i = 0; i <= ages.Length; i++)
+            {
+                Console.WriteLine(ages[i]);
+            }
+        }
+    }
+
+    
+}
+
+
+/*
+ Index is out of bounds - means index is outside of lower and upper limit
+ * 
+ */
