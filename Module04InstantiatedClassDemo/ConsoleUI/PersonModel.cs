a6fa6f4aab0df0a5c727c88a3e27f3645bf0bb90
Udaya <udayarai@hotmail.co.uk>
2024-10-21 19:57:54 +0100

    Initial commit

---
+++ Module04InstantiatedClassDemo/ConsoleUI/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    // Blueprint
+    public class PersonModel        //since it is not static it cannot be directly accessed in Program.cs
+    {
+        //public string firstName;
+        //public string lastName;
+        //public string emailAddress;
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public string EmailAddress { get; set; }
+        public bool HasBeenGreeted { get; set; }
+    }
+}
---
+++ Module04InstantiatedClassDemo/ConsoleUI/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program      
+    {
+        static void Main(string[] args)
+        {
+            //Building a house from the blueprint
+            // PersonModel person = new PersonModel();   //creates a new house from the blueprint 
+
+            //List<PersonModel> people = new List<PersonModel>();  //create a list and instantiate a list
+
+            ////Variable holds the street address
+            //PersonModel person = new PersonModel();
+            //person.firstName = "Tim";
+            //people.Add(person);
+
+
+            //person = new PersonModel();
+            //person.firstName = "Sue";
+            //people.Add(person);
+
+            //foreach (PersonModel p in people)
+            //{
+            //    Console.WriteLine(p.firstName);
+            //}
+
+            List<PersonModel> people = new List<PersonModel>();
+            string firstName = "";
+            
+
+            do
+            {
+                Console.Write("What is your first name (or type exit to stop): ");
+                firstName = Console.ReadLine();
+
+                Console.Write("What is your last name: ");
+                string lastName = Console.ReadLine();
+
+                if (firstName.ToLower() != "exit")
+                {
+                    PersonModel person = new PersonModel();
+                    person.FirstName = firstName;
+                    person.LastName = lastName;
+                    people.Add (person);
+                }
+            } while (firstName.ToLower() != "exit");
+
+            foreach (PersonModel p in people)
+            {
+                ProcessPerson.GreetPerson(p);
+            }
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
+
+
+/*
+Instantiated classes
+class instances are where we put data for short period of time or long period of time
+In general, instantiated classes store data. Static classes are for stateless processing.
+ */
