46effd3cc7c2ba82945e5e201f2351a823dbe8ea
Udaya <udayarai@hotmail.co.uk>
2024-10-25 15:37:19 +0100

    Initial commit

---
+++ Module04PropertyTypesExcercise/ConsoleUI/AddressModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class AddressModel
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
+
+        //full property
+        private string _address;
+
+        public string FullAddress
+        {
+            get 
+            { 
+                return $"{StreetAddress}\n{City}\n{State}\n{PostalCode}";
+            }
+        }
+
+    }
+}
---
+++ Module04PropertyTypesExcercise/ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressModel address = new AddressModel
+            {
+                StreetAddress = "Culver Road",
+                City = "Basingstoke",
+                State = "Hampshire",
+                PostalCode = "RG21 3NJ"
+            };
+
+
+            Console.WriteLine(address.FullAddress);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+/* 
+ Create a class that has properties for street address, city, state,
+and zip code. Then add a FullAddress property that is read-only
+ 
+ 
+ */
