095ccf8df40fa218de40e8ecd34a31f569dc10ee
Udaya <udayarai@hotmail.co.uk>
2024-11-07 22:39:05 +0000

    Initial commit class library added

---
+++ BattleshipLiteApp/BattleshipLiteLibrary/Models/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary.Models
+{
+    public class Enums
+    {
+        // 0 = empty, 1 = ship, 2 = miss, 3 = hit, 4 = sunk
+        public enum GridSpotStatus
+        {
+            Empty,    //we can overwrite but for the most part we do not change
+            Ship,
+            Miss,
+            Hit,
+            Sunk
+        }
+    }
+}
---
+++ BattleshipLiteApp/BattleshipLiteLibrary/Models/GridSpotModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BattleshipLiteLibrary.Models.Enums;
+
+namespace BattleshipLiteLibrary.Models
+{
+    public class GridSpotModel
+    {
+        public string SpotLetter {  get; set; }
+        public int SpotNumber { get; set; }
+        public GridSpotStatus Status { get; set; } = GridSpotStatus.Empty; //by default we want every spot to be empty
+    }
+}
+
+/*
+ For code re-useability we need to use class library
+If we need to change UI then we can use class library
+Class library creates a dll and pdb (helps with debugging)
+ Library cannot be directly run as it does not have a UI
+ */
