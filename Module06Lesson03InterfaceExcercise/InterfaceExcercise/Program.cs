bed8bf9124289a84388e849116a4f24a4759d9c0
Udaya <udayarai@hotmail.co.uk>
2024-12-04 21:03:17 +0000

    Module06Lesson03InterfaceExcercise Addded

---
+++ Module06Lesson03InterfaceExcercise/InterfaceExcercise/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Animal animal = new Animal();
+
+            person1.Run();
+            animal.Run();
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IRun
+    {
+        void Run();
+    }
+
+    public class Person : IRun
+    {
+        public void Run()
+        {
+            Console.WriteLine("running !!!");
+        }
+    }
+
+    public class Animal : IRun
+    {
+        public void Run()
+        {
+            Console.WriteLine($"Animal is running !!!");
+        }
+    }
+} 
+
+
+
+/*
+ Create an IRun interface and apply it to a Person class and an Animal class. Store both types in a List<IRun> object
+ */
