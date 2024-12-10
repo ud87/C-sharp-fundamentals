a8e208e4793c4dbefa30f245c8e92d5d204fed38
Udaya <udayarai@hotmail.co.uk>
2024-12-10 22:39:58 +0000

    Module06Lesson07MethodOverRidingDemoAppExcercise added

---
+++ Module06Lesson07MethodOverRidingDemoAppExcercise/MethodOverRidingDemoAppExcercise/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodOverRidingDemoAppExcercise
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+            
+        }
+    }
+}
+
+
+/*
+Create a person class and override the ToString method in it. 
+Make it return the user's first and last name
+add a abstract method to the person class then override the method in an employee class which inherits from a person
+ */
---
+++ Module06Lesson07MethodOverRidingDemoAppExcercise/MethodOverRidingDemoAppExcercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverRidingDemoAppExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                FirstName = "Udaya",
+                LastName = "Rai",
+                Email = "udayarai@aol.com"
+            };
+
+            Console.WriteLine(person);
+
+
+            Employee employee = new Employee();
+            Console.WriteLine(employee.Introduce());
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Greeting
+    {
+        public abstract string Introduce();
+    }
+
+    public class Employee : Greeting
+    {
+        public override string Introduce()
+        {
+            return $"Hi I am a permanent employee";
+        }
+    }
+
+    public class Contractor : Greeting 
+    {
+        public override string Introduce()
+        {
+            return $"Hi I am a contractor";  
+        }
+    }
+}
+
+
+/*
+Create a person class and override the ToString method in it. 
+Make it return the user's first and last name
+add a abstract method to the person class then override the method in an employee class which inherits from a person
+ */
