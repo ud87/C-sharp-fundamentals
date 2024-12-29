eee0dece7ba7b0a3ca2281e0caa1b5bcbcbae567
Udaya <udayarai@hotmail.co.uk>
2024-12-29 15:19:51 +0000

    Module06Lesson14WrapUpDemo added

--- Module06Lesson14WrapUpDemo/WrapUpDemo/Program.cs
+++ Module06Lesson14WrapUpDemo/WrapUpDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,13 +25,33 @@ namespace WrapUpDemo
                 new CarModel {Manufacturer = "Ford", Model = "Mustang"}
             };
 
+            DataAccess<PersonModel> peopleData = new DataAccess<PersonModel>();
+            peopleData.BadEntryFound += PeopleData_BadEntryFound;
+
+            DataAccess<CarModel> carData = new DataAccess<CarModel>();
+            carData.BadEntryFound += CarData_BadEntryFound;
+
+            peopleData.SaveToCSV(people, @"C:\Temp\SavedFiles\people.csv");  //@ tells us there are no escape characters
+            carData.SaveToCSV(cars, @"C:\Temp\SavedFiles\cars.csv");
+
             Console.ReadLine();
+        }
+
+        private static void CarData_BadEntryFound(object sender, CarModel e)
+        {
+            Console.WriteLine($"Bad Entry found for {e.Manufacturer} {e.Model}");
+        }
+
+        private static void PeopleData_BadEntryFound(object sender, PersonModel e)
+        {
+            Console.WriteLine($"Bad Entry found for {e.FirstName} {e.LastName}");
         }
     }
 
-    public static class DataAccess
+    public class DataAccess<T> where T : new()
     {
-        public static void SaveToCSV<T>(List<T> items) where T:new()   //generic extension method
+        public event EventHandler<T> BadEntryFound;
+        public void SaveToCSV(List<T> items, string filePath)   //generic extension method
         {
             List<string> rows = new List<string>();
             T entry = new T();
@@ -39,11 +60,53 @@ namespace WrapUpDemo
             string row = "";
             foreach (var col in cols)
             {
-                row += $"{col.Name}, ";
+                row += $",{col.Name}";
+            }
+            row = row.Substring(1);  //will start from 1st index
+            rows.Add(row);
+
+            bool badWordDetected = false;
+
+            foreach (var item in items)
+            {
+                row = "";
+                foreach (var col in cols)
+                {
+                    string val = col.GetValue(item, null).ToString();
+
+                    badWordDetected = BadWordDetector(val);
+                    if (badWordDetected == true)
+                    { 
+                        BadEntryFound?.Invoke(this, item);
+                        break;      //break us out of for loop
+                    }
+                    row += $",{val}";
+                }
+
+                if (badWordDetected == false)
+                {
+                    row = row.Substring(1);  //will start from 1st index
+                    rows.Add(row);
+                }
             }
 
-            13:49
+            File.WriteAllLines(filePath, rows);
 
+            
+
+        }
+
+        private bool BadWordDetector(string stringToTest)  //will test if a bad workd is in string
+        {
+            bool output = false;
+            string lowerCaseTest = stringToTest.ToLower();
+
+            if (lowerCaseTest.Contains("darn") || lowerCaseTest.Contains("heck"))
+            {
+                output = true;
+            }
+
+            return output;
         }
     }
 }
